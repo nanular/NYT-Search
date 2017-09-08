494eb4665baa5596592d1345b3b17b6ac608f846
Neil Zimmerman <Neil@MacBook-Pro-13-Mid-2012.local>
2017-09-07 23:27:25 -0500

    working on displaying multiple ajax results

--- assets/javascript/api.js
+++ assets/javascript/api.js
@@ -5,17 +5,15 @@ var toSearch =
 {
 	baseURL: "https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=",
 	apiKey: "f05a7362fdb341b3b69c4aa99b704d34",
-	q: "&?q=donald",
+	q: "&?q=suicide",
 	begin: "&?begin_date=20170101",
 	end: "&?end_date=20171231",
 }
 
 
 
-
 $("#searchButton").click(function()
 {
-	$("#searchResults").show();
 	var queryURL = toSearch.baseURL + toSearch.apiKey + toSearch.q + toSearch.begin + toSearch.end;
 	console.log(queryURL);
 
@@ -24,11 +22,62 @@ $("#searchButton").click(function()
 		url: queryURL,
 		method: "GET"
 	})
-		.done(function(response)
+		.done(function(apireturn)
 		{
-			console.log(response);
+			$("#resultsContainer").show();
+			$("#searchResults").show();
+			$("#searchResultsHeader").show();
 
-			$("#searchResults").append("<h2>NYT Search Results</h2>");
+			console.log("finished fetching data from nytimes api")
+			var results = apireturn.response.docs;
+
+			var articleDiv = $("<div>");
+			var articleTitle = $("<h3>");
+			var shortSummary = $("<p>");
+
+			var i = 0;
+
+			for(var i = 0; i < results.length; i++)
+			{
+
+			var yearPub = (results[i].pub_date).slice(0, 4);
+			var monthPub = (results[i].pub_date).slice(5, 7);
+			var dayPub = (results[i].pub_date).slice(8, 10);
+			var datePublished = $("<p>");
+			datePublished.text("Date Published: " + monthPub + "/" + dayPub + "/" + yearPub);
+			datePublished.css("font-size", "0.7em");
+
+			articleTitle.addClass("display3");
+			articleTitle.text(results[i].headline.main);
+			shortSummary.text(results[i].snippet);
+			shortSummary.css("font-size", "0.7em");
+
+			articleDiv.append(articleTitle);
+			articleDiv.append(datePublished);
+			articleDiv.append(shortSummary);			
+
+			$("#searchResults").append(articleDiv)
+			//$("#searchResults").append("<br><br><br>");
+
+			}
+
+
+
+
+
+
+
+			//for (var i = 0; i < results.length; i++)
+			//{
+				
+
+
+
+
+			//}
+
+			
+
 
 
 
