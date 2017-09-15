d9ccbb94a72e04ad971f92c07e7be55d2be3cd5b
Neil Zimmerman <Neil@MacBook-Pro-13-Mid-2012.local>
2017-09-15 13:08:44 -0500

    update

--- assets/javascript/api.js
+++ assets/javascript/api.js
@@ -69,63 +69,6 @@ $("#searchButton").click(function()
 
 	})
 
-/*	$.ajax
-	({
-		url: queryURL,
-		method: "GET"
-	})
-		.done(function(apireturn)
-		{
-			$("#resultsContainer").show();
-			$("#searchResults").show();
-			$("#searchResultsHeader").show();
-
-			console.log("finished fetching data from nytimes api")
-			var results = apireturn.response.docs;
-
-			var articleDiv = $("<div>");
-			var articleTitle = $("<h3>");
-			var shortSummary = $("<p>");
-
-			var i = 0;
-
-			for(var i = 0; i < results.length; i++)
-			{
-
-			var yearPub = (results[i].pub_date).slice(0, 4);
-			var monthPub = (results[i].pub_date).slice(5, 7);
-			var dayPub = (results[i].pub_date).slice(8, 10);
-			var datePublished = $("<p>");
-			datePublished.text("Date Published: " + monthPub + "/" + dayPub + "/" + yearPub);
-			datePublished.css("font-size", "0.7em");
-
-			articleTitle.addClass("display3");
-			articleTitle.text(results[i].headline.main);
-			shortSummary.text(results[i].snippet);
-			shortSummary.css("font-size", "0.7em");
-
-			articleDiv.append(articleTitle);
-			articleDiv.append(datePublished);
-			articleDiv.append(shortSummary);			
-
-			$("#searchResults").append(articleDiv)
-			//$("#searchResults").append("<br><br><br>");
-
-			}*/
-
-				
-
-
-
-
-			//}
-
-			
-
-
-	//	});
-
-
 })
 
 
@@ -135,29 +78,4 @@ $("#searchButton").click(function()
 
 
 
-
-
-
-
-
-
 })
-
-
-
-
-
-
-
-// var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
-// url += '?' + $.param({
-//   'api-key': "f05a7362fdb341b3b69c4aa99b704d34"
-// });
-// $.ajax({
-//   url: url,
-//   method: 'GET',
-// }).done(function(result) {
-//   console.log(result);
-// }).fail(function(err) {
-//   throw err;
-// });
